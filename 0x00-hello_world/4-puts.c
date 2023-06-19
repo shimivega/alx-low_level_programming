aa40a1c7996033bc7da49e8bd7cf6ca46183baaa
shimiveg <shimimsauden@gmail.com>
2023-06-19 10:19:23 -0700

    Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {     
-        puts("\"programming is like buliding a multilingual puzzle
+        puts("\"programming is like building a multilingual puzzle
 ");     
         return (0);
 }	
